ffce8110bb6dd9a491da0f8e75c5ea08e918e4e0
kkhashayar <frankdezz@gmail.com>
2025-02-01 14:48:16 +0100

    added engine host service to run the winboard in background

--- Engine_Core/EngineAPI/Program.cs
+++ Engine_Core/EngineAPI/Program.cs
@@ -1,8 +1,11 @@
+using Engine_API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHostedService<EngineHostService>(); 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Engine_Core/EngineAPI/Services/EngineHostService.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Hosting;
+
+namespace Engine_API.Services;
+
+public class EngineHostService : BackgroundService
+{
+    private Process? _engineProcess;
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.Run(() =>
+                {
+                    _engineProcess = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "D:\\Data\\Repo\\K_Chess_2\\Engine_Core\\Engine_UI\\Program.cs",
+                            Arguments = "run",
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                            UseShellExecute = false,
+                            CreateNoWindow = true
+                        }
+                    };
+                    _engineProcess.Start();
+                    _engineProcess.WaitForExit();
+                });
+    }
+
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _engineProcess?.Kill();
+        await base.StopAsync(cancellationToken);    
+    }
+}
