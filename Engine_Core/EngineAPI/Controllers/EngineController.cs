160b2b1d657d5f16acdd3eadf747546a98eb02ae
kkhashayar <frankdezz@gmail.com>
2025-02-01 12:58:09 +0100

    Implement the controller, Still just returning predefined responces from services, but swagger works

---
+++ Engine_Core/EngineAPI/Controllers/EngineController.cs
@@ -0,0 +1,47 @@
+﻿using Engine_API.Interfaces;
+using Engine_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Engine_API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EngineController : ControllerBase
+{
+    private readonly IEngineService _engineService;
+
+    public EngineController(IEngineService engineService)
+    {
+        _engineService = engineService;
+    }
+
+
+
+    [HttpGet("newgame")]
+    public async Task<ActionResult<string>> GetNewGame()
+    {
+        var response = await _engineService.GetNewGame();
+        if (response == null)
+            return NotFound();
+        return Ok(response);
+    }
+
+    [HttpGet("stopgame")]
+    public async Task<ActionResult<string>> StopGame()
+    {
+        var response = await _engineService.StopGame();
+        if (response == null)
+            return NotFound();
+        return Ok(response);
+    }
+
+    [HttpPost("sendmove")]
+    public async Task<ActionResult<string>> SendMove([FromBody] Move move)
+    {
+        var response = await _engineService.SendMove(move);
+        if (response == null)
+            return NotFound();
+        return Ok(response);
+    }
+
+}
