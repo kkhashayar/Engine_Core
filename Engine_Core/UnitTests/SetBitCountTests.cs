395fc9fb5a19f721a49e36e0eded54b7e88b68d9
kkhashayar <frankdezz@gmail.com>
2025-04-03 12:59:37 +0200

    Added Fluentassertion nuget to Unit test project Added set bit unit test for Kernighan and classic method.

---
+++ Engine_Core/UnitTests/SetBitCountTests.cs
@@ -0,0 +1,47 @@
+﻿using Engine_Core;
+using FluentAssertions;
+
+namespace UnitTests;
+
+
+public class SetBitCountTests
+{
+
+    /*
+     *      ********************* Brian Kernighan’s Way Bit Hack for Counting Set Bits *********************
+     *      
+     *      bitboard &= bitboard -1;
+     *      by incrementing one set bit in each loop until there is no set bit left
+     *      that's why we dont need to loop the whole chain and we have faster counter.
+     *      
+     */
+
+    [Theory]
+    [InlineData(0b0UL, 0)]
+    [InlineData(0b1UL, 1)]
+    [InlineData(0b1011UL, 3)]
+    [InlineData(0b11111111UL, 8)]
+    [InlineData(0xFFFFFFFFFFFFFFFFUL, 64)]
+    public void CountBits_Returns_Correct_Set_Bit_Count(ulong bitboard, int expectedCountNumber)
+    {
+        var sut = Globals.CountBits(bitboard);
+        sut.Should().Be(expectedCountNumber);
+    }
+
+
+    [Theory]
+    [InlineData(0b0UL, 0)]
+    [InlineData(0b1UL, 1)]
+    [InlineData(0b1011UL, 3)]
+    [InlineData(0b11111111UL, 8)]
+    [InlineData(0xFFFFFFFFFFFFFFFFUL, 64)]
+    public void CountBits_Returns_Correct_Set_Bit_Count_Classic(ulong bitboard, int expectedCountNumber)
+    {
+        var sut = Globals.CountSetBitClassic(bitboard);     
+        sut.Should().Be(expectedCountNumber);
+    }
+
+}
+
+
+
